86e6b30af9033d0d5febd21ac57bfc78d09b98da
CrispusCrew <crispuscrew71@gmail.com>
2025-01-21 19:01:00 +0300

    autoinstaller

--- README.md
+++ README.md
@@ -1,13 +1,107 @@
-# Important!
-You should correct this lines:
+# Description
 
-in *hypr/hyprland.conf* set up your screen 
-```
-monitor=,1920x1080@60,auto,1
+This is auto-install .config for **Fedora 41** with Hyprland (Wayland), Waybar, and additional utilities for enhanced desktop experience.
+
+# Install Guide
+
+1. Download actual version of [**Fedora Server Network Install iso**](https://fedoraproject.org/server/download)
+
+2. Create a bootable USB drive by, for example, **dd** comand (Linux) or [**Rufus**](https://rufus.ie/) (Windows)
+
+3. Using your USB, install Fedora Workstation only with network drives (u maybe must use anaconda repo (https://repo.anaconda.com/pkgs))
+
+4. After completing the system installation, log in with TTY and install git package by:
+```bash
+sudo dnf install git -y
 ```
 
-in *waybar/config*, if necessary, uncomment the following line:
+5. Next, you download this repo:
+```bash
+git clone https://github.com/CrispusCrew/.config.git
+```
+
+6. And start bash script
+```bash
+cd .config
+sudo chmod +x config_auto.sh
+sudo ./config_auto.sh
+```
+
+7. You must install your GPU drivers by yourself, otherwise wayland-session may crash. If u have nvidia GPU, in my expirience, best way to get it - by RPM Fusion. For it:
+```bash
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install akmod-nvidia
+sudo reboot
+```
+
+8. Reboot your pc/laptop. Log in by TTY (press Ctrl+Alt+F2 if necessary), and write
+```bash
+Hyprland
+```
+
+# Enjoy
+Your system is now configured with Hyprland and customized utilities. Explore and enjoy your beautiful new Linux desktop experience!
+
+# Hotkeys
+
+You're always can change it in ~/.config/hypr/hyprland.conf
+(super = $mainMod = Windows button = ⌘ Command)
+
+### Main hotkeys
+
+| Hotkey         | Action                     |
+|----------------|----------------------------|
+| super + C      | close focus window         |
+| super + Q      | open terminal              |
+| super + R      | open apps select menu      |
+| super + V      | win+V equivalent           |
+| super + S      | win + shift + s equivalent |
+| super + L      | win + L equivalent         |
+| super + arrows | toggle focus               |
+| super + LMB   | move window            |
+| super + RMB   | resize window          |
+
+### Workspaces
+
+| Hotkey                | Action                                   |
+|-----------------------|------------------------------------------|
+| super + (1-9)         | switch workspace (1-9)                   |
+| super + (1-9) + shift | bring focus window to (1-9) workspace    |
+| super + A             | back to previous workspace               |
+| super + mouse scroll  | scroll workspaces                        |
+| super + E             | switch to overflow workspace             |
+| super + shift + E     | bring focus window to overflow workspace |
+| super + D             | win + D equivalent                       |
+
+
+### Additional hotkeys
+
+| Hotkey      | Action                    |
+|-------------|---------------------------|
+| super + M   | close Hyprland session    |
+| super + F11 | fullscreen focus window   |
+| super + Tab | alt + Tab equivalent      |
+| super + P   | pseudotile window         |
+| super + J   | toggle split orientation  |
+| super + W   | toggle waybar             |
+| printscreen | screenshot save like file |
+| super + F   | open file manager         |
+| super + [   | wallpapers carousel on    |
+| super + ]   | wallpapers off            |
+
+
+# P.S
+
+- U can click and double click to waybar icons for some things (just try)
+
+- U can use multimedia (XF86) buttons for volume, media and screen control
+
+- in *waybar/config*, if necessary, uncomment and complete the following line:
 ```json
-//"interface": "your-interface",
+//"interface": "paste-your-interface-here",
 ```
-and configure your Ethernet interface
+
+# P.P.S.
+
+If you have any suggestions for improvements and/or adding something new to this repository, please feel free to fork it or message me on Telegram: https://t.me/gabzetdinov
---
+++ config_auto.sh
@@ -0,0 +1,141 @@
+if [ "$(id -u)" -ne 0 ]; then
+    echo "[ERR] This script must be run as root (sudo). Exiting."
+    exit 1
+fi
+
+echo "[INFO] dnf install..."
+
+dnf install -y \
+hyprland wayfire wlroots kitty mc wofi grim slurp waybar wl-copy wl-paste \
+playerctl brightnessctl pactl xdotool libinput xbindkeys libva wlr-randr \
+&& echo "[INFO] dnf install complete." || echo "[ERR] dnf install finish with error, pls repair it after script completing"
+
+
+echo "[INFO] Others bin install..."
+
+if [ -d "./config_files/bin" ]; then
+    cp ./config_files/bin/* /usr/bin/ \
+    && echo "[INFO] Binary install complete." || echo "[ERR] Binary install finished with errors."
+else
+    echo "[ERR] Directory ./config_files/bin not found. Skipping binary installation."
+fi
+
+
+echo "[INFO] Configs install..."
+
+if [ -d "./config_files/configs" ]; then
+    cp -r ./config_files/configs/* ./ \
+    && echo "[INFO] Configs install complete." || echo "[ERR] Configs install finished with errors."
+else
+    echo "[ERR] Directory ./config_files/configs not found. Skipping configs installation."
+fi
+
+
+echo "[INFO] Monitor setup:"
+
+if [ ! -d "/sys/class/drm" ]; then
+    echo "[ERR] /sys/class/drm not found. Ensure DRM is supported by your kernel."
+    exit 1
+fi
+
+total_width=0
+for device in /sys/class/drm/*-*/status; do
+    if [ "$(cat "$device")" != "connected" ]; then
+        continue
+    fi
+    name=$(basename "$(dirname "$device")")
+    modes_file="$(dirname "$device")/modes"
+
+    if [ -f "$modes_file" ]; then
+        max_mode=$(cat "$modes_file" | sort -V | tail -n 1)
+        echo "[INFO] Monitor detected: $name"
+        echo "[INFO] Max mode: $max_mode"
+
+        width=$(echo "$max_mode" | cut -d'x' -f1)
+    else
+        echo "[ERR] Monitor $name has no modes file. You must edit hypr/hyprland.config manually to configure this monitor later."
+        continue
+    fi
+
+    read -p "Is this correct? y/n: " answer
+    while true; do
+        case $answer in
+            [Yy]* )
+                read -p "Insert monitor scale (example, 2.0): " scale
+                if [[ ! "$scale" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+                    echo "[ERR] Incorrect scale, try again"
+                    continue
+                fi
+
+                echo "[INFO] Adding configuration for $name..."
+                sed -i "1i monitor = $name, $max_mode, ${total_width}x0, $scale" hypr/hyprland.conf
+                total_width=$((total_width + width))
+                break
+                ;;
+            [Nn]* )
+                echo "[INFO] You must edit hypr/hyprland.config manually to configure this monitor later."
+                break
+                ;;
+            * )
+                echo "[ERR] Incorrect answer, try again."
+                continue
+                ;;
+        esac
+    done
+done
+
+echo "[INFO] Monitor setup complete"
+
+
+echo "[INFO] Don't forget install drivers for your GPU for correct system works"
+
+echo '[INFO] After this, reboot your system, log in with TTY and type "Hyprland"'
+
+read -p "Want to configure wallpapers path? (You can do this manually later) y/n: " answer
+while true; do
+    case $answer in
+        [Yy]* )
+            read -p "Type wallpaper path without / at the end (you can use \$HOME for ~/): " wallpaper_path
+            if [ -f "${wallpaper_path/#\~/$HOME}" ]; then
+                echo "[INFO] Adding configuration for wallpaper..."
+                sed -i "1i \$wallpaper_path = '$wallpaper_path'" hypr/hyprland.conf
+            else
+                echo "[ERR] Invalid wallpaper path. Please configure it manually later."
+            fi
+            break
+            ;;
+        [Nn]* )
+            break
+            ;;
+        * )
+            echo "[ERR] Incorrect answer, try again."
+            continue
+            ;;
+    esac
+done
+
+read -p "Want to configure screenshots path? (You can do this manually later) y/n: " answer
+while true; do
+    case $answer in
+        [Yy]* )
+            read -p "Type screenshots path without / at the end (you can use \$HOME for ~/): " screenshots_path
+            if [ -f "${wallpaper_path/#\~/$HOME}" ]; then
+                echo "[INFO] Adding configuration for screenshots..."
+                sed -i "1i \$screenshot_path = '$screenshots_path'" hypr/hyprland.conf
+            else
+                echo "[ERR] Invalid screenshots path. Please configure it manually later."
+            fi
+            break
+            ;;
+        [Nn]* )
+            break
+            ;;
+        * )
+            echo "[ERR] Incorrect answer, try again."
+            continue
+            ;;
+    esac
+done
+
+echo "[INFO] Enjoy your new beautiful system"
+
