c644b438ab7dac0fbe04ac043f78653b344ec1bd
CrispusCrew <crispuscrew71@gmail.com>
2025-02-10 21:38:34 +0300

    waybar update & minors

--- config_auto.sh
+++ config_auto.sh
@@ -5,14 +5,19 @@ fi
 
 user_home=$(eval echo "~$SUDO_USER")
 
+echo "[INFO] dnf copr  install..."
+
+dnf install dnf-plugins-core -y
+dnf copr enable elxreno/jetbrains-mono-fonts -y \
+&& echo "[INFO] dnf copr install complete." || echo "[ERR] dnf copr install finish with error, pls repair it after script completing"
+
 echo "[INFO] dnf install..."
 
 dnf install -y \
 hyprland wayfire wlroots kitty mc wofi grim slurp waybar wl-copy wl-paste \
 playerctl brightnessctl pactl xdotool libinput xbindkeys libva wlr-randr \
-pavucontrol blueman swaylock \
+pavucontrol blueman swaylock jetbrains-mono-fonts pulseaudio-utils\
 && echo "[INFO] dnf install complete." || echo "[ERR] dnf install finish with error, pls repair it after script completing"
-
 
 echo "[INFO] Others bin install..."
 
