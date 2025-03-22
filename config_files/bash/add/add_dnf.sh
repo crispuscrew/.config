7c2ca669bab42f891221d0f4bf39f6277254cf8b
CrispusCrew <crispuscrew71@gmail.com>
2025-03-22 18:04:22 +0300

    rpi-imager add to additional pkgs

--- config_files/bash/add/add_dnf.sh
+++ config_files/bash/add/add_dnf.sh
@@ -1,5 +1,5 @@
 echo "[INFO] Installing dnf packages..."
-dnf install -y flatpak jq curl speedcrunch gnome-keyring libsecret-* qemu-system-arm libvirt virt-manager bottles samba samba-common samba-client
+dnf install -y flatpak jq curl speedcrunch gnome-keyring libsecret-* qemu-system-arm libvirt virt-manager bottles samba samba-common samba-client rpi-imager
 if [ $? -ne 0 ]; then
     echo "[ERR] dnf packages installation finished with error, please repair it after script completion"
     exit -1
