1766042aa11b2690dfeee96918a65f4f8b9c4b4d
crispuscrew <crispuscrew71@gmail.com>
2025-04-15 21:46:48 +0300

    jetbrains fonts-add

--- config_files/bash/add/add_dnf.sh
+++ config_files/bash/add/add_dnf.sh
@@ -1,6 +1,6 @@
 echo "[INFO] Installing dnf packages..."
 dnf install -y flatpak jq curl speedcrunch gnome-keyring libsecret-* qemu-system-arm libvirt virt-manager \
-    bottles samba samba-common samba-client rpi-imager \
+    bottles samba samba-common samba-client rpi-imager jetbrains-mono-fonts-all \
     cmake ninja-build gcc-c++ qt6-qtbase-devel qt6-qtdeclarative-devel gdb
 if [ $? -ne 0 ]; then
     echo "[ERR] dnf packages installation finished with error, please repair it after script completion"
